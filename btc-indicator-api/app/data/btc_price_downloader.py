f8fbd54fb39e3a1ad2eff3a917a9799b61befad9
F-Korpik <korpik.franciszek@gmail.com>
2025-07-11 17:09:41 +0200

    changing chart length from day to 30 days, testing new branch

--- btc-indicator-api/app/data/btc_price_downloader.py
+++ btc-indicator-api/app/data/btc_price_downloader.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def download_btc_price_history(url, currency: str = "usd", output_file: str = "data/btc_price_history.json") -> None:
     now = datetime.now()
@@ -8,7 +8,7 @@ def download_btc_price_history(url, currency: str = "usd", output_file: str = "d
 
     params = {
         "vs_currency": currency,
-        "from": int(start_of_day.timestamp()),
+        "from": int((start_of_day - timedelta(days=30)).timestamp()),
         "to": int(now.timestamp()),
     }
 
@@ -20,4 +20,4 @@ def download_btc_price_history(url, currency: str = "usd", output_file: str = "d
     with open(output_file, "w") as f:
         json.dump(data["prices"], f, indent=2)
 
-    print(f"✅ Dane z dzisiaj zapisane do: {output_file}")
+    print(f"✅ Dane z zapisane do: {output_file}")
