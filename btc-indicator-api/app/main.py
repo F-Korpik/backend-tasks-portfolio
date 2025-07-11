46efe693082516ecbe64ddb4bbb663de3ca57caa
F-Korpik <korpik.franciszek@gmail.com>
2025-07-11 16:52:20 +0200

    Added function to download btc price history and endpoint to put in on chart (localhost)

--- btc-indicator-api/app/main.py
+++ btc-indicator-api/app/main.py
@@ -1,13 +1,37 @@
-# app/main.py
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import json
+from pathlib import Path
 
-from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="app/templates")
+
+from app.data.btc_price_downloader import download_btc_price_history
+
 
 app = FastAPI()
 
-@app.get("/ping")
-def ping():
-    return {"message": "pong"}
+URL = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range"
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_chart(request: Request):
+    file_path = Path("app/data/btc_price_history.json")
+    with file_path.open() as f:
+        data = json.load(f)
+
+    labels = [int(pair[0]) for pair in data]
+    values = [pair[1] for pair in data]
+
+    return templates.TemplateResponse("chart.html", {
+        "request": request,
+        "labels": labels,
+        "values": values
+    })
+
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+
+    download_btc_price_history(URL, "usd" )
+
