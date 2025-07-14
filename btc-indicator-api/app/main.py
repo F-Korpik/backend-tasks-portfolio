4728792a5797fab04a4e9f9551400ab2fd905542
F-Korpik <korpik.franciszek@gmail.com>
2025-07-14 14:34:40 +0200

    Added full price data and automated updates

--- btc-indicator-api/app/main.py
+++ btc-indicator-api/app/main.py
@@ -4,25 +4,29 @@ from fastapi.templating import Jinja2Templates
 import json
 from pathlib import Path
 
-from fastapi.templating import Jinja2Templates
-templates = Jinja2Templates(directory="app/templates")
 
-from app.data.btc_price_downloader import download_btc_price_history
-
+from app.data.update_price_info import update_price_info
+from app.data.csv_to_json import csv_to_json
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="app/templates")
 
 URL = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range"
 
 
 @app.get("/", response_class=HTMLResponse)
 async def read_chart(request: Request):
+    update_price_info(URL, "usd")
+
     file_path = Path("app/data/btc_price_history.json")
     with file_path.open() as f:
         data = json.load(f)
 
-    labels = [int(pair[0]) for pair in data]
-    values = [pair[1] for pair in data]
+    data.reverse()
+
+    labels = [row[0] for row in data]
+    values = [float(row[1]) for row in data]
 
     return templates.TemplateResponse("chart.html", {
         "request": request,
@@ -30,8 +34,14 @@ async def read_chart(request: Request):
         "values": values
     })
 
+@app.get("/csv_to_json")
+def convert_csv_to_json():
+    csv_to_json("app/data/btc_price_history.csv", "app/data/btc_price_history.json")
+
 
 if __name__ == "__main__":
+    # csv_to_json("data/btc_price_history.csv", "data/btc_price_history.json")
+    # update_price_info(URL, "usd")
 
-    download_btc_price_history(URL, "usd" )
+    pass
 
